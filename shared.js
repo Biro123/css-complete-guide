1a1ed5c179c64f11e6acc8b63ca11b8e61fa7327
Biro123 <biro123@ntlworld.com>
2025-09-22 12:02:34 +0100

    Close modal on click

--- shared.js
+++ shared.js
@@ -1,14 +1,23 @@
 // select beckdrop (by finding first element with .backdrop css selector)
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalCloseButton = document.querySelector('.modal__action--negative');
 var selectPlanButton = document.querySelectorAll('.plan button')
 
 // console.dir(selectPlanButton);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
 
 selectPlanButton.forEach(button => {
   button.addEventListener('click', function() {
     backdrop.style.display = 'block';
     modal.style.display = 'block';
   })
-})
+});
+
+backdrop.addEventListener('click', closeModal);
+modalCloseButton.addEventListener('click', closeModal);
 
